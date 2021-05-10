686b2832c641e6f69684ac3bd3b3f70c70f8991f
BoardGame <naveen.rtw1595@gmail.com>
2021-05-10 16:32:13 +0530

    Main added

---
+++ Assets/Scripts/LeaderBoard.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class LeaderBoard : MonoBehaviour
+{
+    public List<GameObject> polledObject;
+    public GameObject objectToPool;
+    public static Object sharedInstance;
+    [SerializeField] private int amtToPool;
+
+    private void Awake()
+    {
+        sharedInstance = this;
+    }
+
+    private void Start()
+    {
+        polledObject = new List<GameObject>();
+        GameObject temp;
+        for(int i=0;i<amtToPool;i++)
+        {
+            temp = Instantiate(objectToPool);
+            temp.SetActive(false);
+            polledObject.Add(temp);
+        }
+    }
+
+    public GameObject GetPooledObject()
+    {
+        for(int i=0;i<amtToPool;i++)
+        {
+            if(!polledObject[i].activeInHierarchy)
+            {
+                polledObject[i].SetActive(true);
+                return polledObject[i];
+            }
+        }
+        return null;
+    }
+
+}
